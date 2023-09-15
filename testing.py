9faeec21c1bdeec79e864d345189a5259230738a
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-14 20:24:29 -0500

    unittest update

--- testing.py
+++ testing.py
@@ -2,8 +2,20 @@ import unittest
 
 class TestSum(unittest.TestCase):
 
+    def setUp(self):
+        self.list = [2,2,2]
+
     def test_sum_list(self):
-        self.assertEqual(sum([6,6,6]), 18)
+        self.assertEqual(sum(self.list), 6)
+
+    def test_multiply_list(self):
+        self.assertEqual((self.list[1]* self.list[2]), 4)
+
+    @unittest.skip("skipping")
+    def test_divide_list(self):
+        self.assertAlmostEqual((self.list[0]/ self.list[1]), 1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
