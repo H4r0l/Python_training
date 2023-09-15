2fad5a39c7eac468e7f0637e1cf8346b4fa25c4c
Har0l <65699208+H4r0l@users.noreply.github.com>
2023-09-14 20:57:15 -0500

    Testing some objects

--- test_objects.py
+++ test_objects.py
@@ -12,3 +12,13 @@ class TestBasic(unittest.TestCase):
         for object in self.data:
             item = Item(object.get("id"), object.get("name"), object.get("useful"))
             self.objects.append(item)
+
+    def test_is_useful(self):
+        for object in self.objects:
+            self.assertEqual(object.is_useful(), "The object is useful")
+
+    def tearDown(self):
+        del(self.objects)
+
+if __name__ == '__main__':
+    unittest.main()
